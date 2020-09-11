155af93fef1867499bff5153ca1c86345e2c201d
Zhichao Cao <caozcthu@gmail.com>
2020-09-10 21:19:40 -0400

    lab01

---
+++ Labs/lab01/Infinite.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int count = 0;
+    while (1) {
+        printf("Can't stop me now!!");
+        printf(" %d\n", count);
+        ++count;
+    }
+    return 0;
+}
