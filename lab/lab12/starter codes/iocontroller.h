1bb1af65ccdda874bdc19fc68deca46265d403ea
manny31415926 <liumanni12@gmail.com>
2020-12-01 02:43:54 -0500

    lab12 material

---
+++ lab/lab12/starter codes/iocontroller.h
@@ -0,0 +1,9 @@
+#ifndef IOCONTROLLER_H
+#define IOCONTROLLER_H
+
+char getType(); 
+float getInput(char type); 
+void printSphere(float volume, float radius);
+void printCube(float volume, float length);
+
+#endif
